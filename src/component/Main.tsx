cae2093b906779974e5cb26a88b9d88e48770ba1
David Galstyan <david.gal007@hotmail.com>
2022-07-09 18:42:22 -0300

    done v2

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -9,18 +9,23 @@ const Main: React.FC = () => {
         <div className="hero__inner">
           <div className="hero__content">
             <h1 className="hero__title">
-              We serve high quality tatoo of all kinds.
+              Osez franchir le pas. Rejoignez l'univers du tatouage.
             </h1>
             <p className="hero__description">
-              je suis infirmière , tatoueuse reconvertie. en plus de tatouages
-              personnalisés , je propose une experience,
+              Lady Numen , Infirmiere tatoueuse, vous propose en plus d'un
+              tatouage entierement personnalisé, une experience, une
+              realisation de votre projet entierement guidée par l'amour de son
+              art et la maitrise de ses aiguilles.
             </p>
-            <HashLink to="#contact" className="hero__btn-plus">Savoir plus</HashLink>
+            <HashLink to="#contact" className="hero__btn-plus">
+              Savoir plus
+            </HashLink>
             <div className="hero__information">
               <article className="subinfo">
                 <h5 className="subinfo__title">Horaires d'ouverture</h5>
                 <span className="subinfo__pfs">
-                  Du lundi au vendredi | 12h a 17h30. possibilité de rdv tout les jours entre 8h et 20h
+                  Du lundi au vendredi | 12h a 17h30. possibilité de rdv tout
+                  les jours entre 8h et 20h
                 </span>
               </article>
               <article className="subinfo">
